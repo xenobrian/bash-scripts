4164ce8e453c139aa3c2165d6d5e67cb58f79aa1
Brilian <briliankrisna27@gmail.com>
2024-07-30 20:09:07 +0700

    Fixed some faulty codes

--- docker-ubuntu.sh
+++ docker-ubuntu.sh
@@ -14,17 +14,17 @@ function dockerInstall {
         apt install ca-certificates curl -y
 
         for i in {1..3}; do
-            install -m -d 0755 /etc/apt/keyrings
+            install -m 0755 -d /etc/apt/keyrings
         done
 
         curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
         chmod a+r /etc/apt/keyrings/docker.asc
 
-        echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc]https://download.docker.com/linux/ubuntu \
+        echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
         $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 
         apt update;
-        apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+        apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
         service docker start
 }
@@ -37,8 +37,6 @@ function userAdd {
 
 dockerInstall
 
-while true; do
-    read -rp "User to add to the 'docker' group. Or 'none' to not add another user : " username
-done
 
+read -rp "User to add to the 'docker' group. Or 'none' to not add another user : " username
 userAdd "username"
