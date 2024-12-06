c00313242f4c99500a0146a0bbb1af76e38afd46
Brilian <briliankrisna27@gmail.com>
2024-12-06 13:49:55 +0700

    Increased Linux distributions coverage to match the official docs

--- docker-ubuntu.sh
+++ docker-ubuntu.sh
@@ -1,74 +1,104 @@
 #!/bin/bash
+echo "Currently only supports"
 
 if (( $EUID != 0 )); then
     echo "Please execute as root.";
     exit;
 fi
 
-linux_distro = ''
+function AptDockerInstall {
+    local linux_distro="$1"
 
-case "$(lsb_release)" in
+    for old_package in docker.io docker-compose docker-compose-v2 docker-doc podman-docker containerd runc; do
+        apt remove $old_package -y
+    done
+
+    apt update;
+    apt install ca-certificates curl -y
+
+    for i in {1..2}; do
+        install -m 0755 -d /etc/apt/keyrings
+    done
+
+    curl -fsSL https://download.docker.com/linux/$linux_distro/gpg -o /etc/apt/keyrings/docker.asc
+    chmod a+r /etc/apt/keyrings/docker.asc
+
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/$linux_distro \
+    $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+    apt update;
+    apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+    service docker start
+}
+
+function RpmDockerInstall {
+    local linux_distro="$1"
+    case "$linux_distro" in
+        "rhel" | "fedora" | "centos")
+        packagemanager=dnf;;
+
+        "sles")
+        packagemanager=zypper;;
+    esac
+
+    if [[ $linux_distro = "sles" ]]; then
+        opensuse_repo="https://download.opensuse.org/repositories/security:/SELinux/openSUSE_Factory/security:SELinux.repo"
+        zypper addrepo $opensuse_repo
+        zypper addrepo https://download.docker.com/linux/sles/docker-ce.repo
+    else
+        dnf -y install dnf-plugins-core
+        dnf config-manager --add-repo https://download.docker.com/linux/$linux_distro/docker-ce.repo
+    fi
+
+    $packagemanager remove docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-engine -y
+
+    if [[ $linux_distro = "fedora" ]]; then
+        dnf remove docker-selinux docker-engine-selinux -y
+    elif [[$linux_distro = "sles" ]]; then
+        zypper remove runc;
+    fi
+
+    $packagemanager install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+    systemctl enable --now docker
+}
+
+case "$(lsb_release -is)" in
     "Debian")
         echo "You are running Debian"
-        linux_distro = "Debian";;
+        AptDockerInstall "debian";;
 
     "Ubuntu")
         echo "You are running Ubuntu"
-        linux_distro = "Ubuntu";;
+        AptDockerInstall "ubuntu";;
 
-    "AlmaLinux" | "Rocky" | "RedhatEnterpriseServer" |  "CentOS")
+    "Fedora")
+        echo "You are running a Fedora."
+        RpmDockerInstall "fedora";;
+
+    "openSUSE" | "SUSE")
+        echo "You are running openSUSE, or SLES."
+        RpmDockerInstall "sles";;
+
+    "RedhatEnterpriseServer")
+        echo "You are running RHEL."
+        RpmDockerInstall "rhel";;
+
+    "CentOS")
+        echo "You are running CentOS"
+        RpmDockerInstall "centos";;
+
+    "AlmaLinux" | "Rocky" )
         echo "You are running a RHEL or its derivative."
-        linux_distro = "RHEL-like";;
-
-    "Fedora" | "openSUSE")
-        echo "You are running a non-RHEL, rpm-using Linux distribution."
-        linux_distro = "rpm-using";;
+        RpmDockerInstall "rhel";;
 esac
-
-function dockerInstall {
-        local packagemanager = ''
-
-        if [[ $linux_distro = "Debian" ]] || [[ $linux_distro = "Ubuntu" ]]; then
-            packagemanager='apt'
-        elif [[ $linux_distro = "RHEL-like"]]; then
-            packagemanager='dnf'
-            dockerRHELinstall
-        elif [[ linux_distro = "rpm-using"]]; then
-            packagemanager='dnf'
-            dockerRPMinstall
-        fi
-
-        for old_package in docker.io docker-compose docker-compose-v2 docker-doc podman-docker containerd runc; do
-            apt remove $old_package -y
-        done
-
-        apt update;
-        apt install ca-certificates curl -y
-
-        for i in {1..2}; do
-            install -m 0755 -d /etc/apt/keyrings
-        done
-
-        curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-        chmod a+r /etc/apt/keyrings/docker.asc
-
-        echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-        $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-        apt update;
-        apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
-
-        service docker start
-}
 
 function userAdd {
     local user="$1"
     usermod -aG docker $user
     grep docker /etc/group
 }
-
-dockerInstall
-
 
 read -rp "User to add to the 'docker' group. Or 'none' to not add another user : " username
 userAdd "$username"
